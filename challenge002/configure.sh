da61bfd0d9f0bffdec2d5aef7cae8b69f08e7563
Michal Rudz <rat12@o2.pl>
2021-06-20 23:05:43 +0200

    Change build image

--- challenge002/configure.sh
+++ challenge002/configure.sh
@@ -1,15 +1,2 @@
 #!/bin/sh
 launch.sh
-apt update 
-apt install kubeadm=1.15.0-00 kubectl=1.15.0-00 kubelet=1.15.0-00 -y
-kubeadm upgrade apply v1.15.0 -y
-systemctl restart kubelet.service
-apt install kubeadm=1.16.0-00 kubectl=1.16.0-00 kubelet=1.16.0-00 -y
-kubeadm upgrade apply v1.16.0 -y
-systemctl restart kubelet.service
-apt install kubeadm=1.17.0-00 kubectl=1.17.0-00 kubelet=1.17.0-00 -y
-kubeadm upgrade apply v1.17.0 -y
-systemctl restart kubelet.service
-apt install kubeadm=1.18.0-00 kubectl=1.18.0-00 kubelet=1.18.0-00 -y
-kubeadm upgrade apply v1.18.0 -y
-systemctl restart kubelet.service
